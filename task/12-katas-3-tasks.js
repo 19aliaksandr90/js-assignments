7808b2e05f04043d8cda1f076e69a6db18fc32ff
aorgish <aorgish>
2016-03-08 16:08:13 +0300

    Add urlShortener kata

--- task/12-katas-3-tasks.js
+++ task/12-katas-3-tasks.js
@@ -66,8 +66,42 @@ function getMostProfitFromStockQuotes(quotes) {
     throw new Error('Not implemented');
 }
 
+
+/**
+ * Class representing the url shorting helper.
+ * Feel free to implement any algorithm, but do not store link in the key\value stores.
+ * The short link can be at least 1.5 times shorter than the original url.
+ * 
+ * @class
+ *
+ * @example
+ *    
+ *     var urlShortener = new UrlShortener();
+ *     var shortLink = urlShortener.encode('https://en.wikipedia.org/wiki/URL_shortening');
+ *     var original  = urlShortener.decode(shortLink); // => 'https://en.wikipedia.org/wiki/URL_shortening'
+ * 
+ */
+function UrlShortener() {
+    this.urlAllowedChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"+
+                           "abcdefghijklmnopqrstuvwxyz"+
+                           "0123456789-_.~!*'();:@&=+$,/?#[]";
+}
+
+UrlShortener.prototype = {
+
+    encode: function(url) {
+        throw new Error('Not implemented');
+    },
+    
+    decode: function(code) {
+        throw new Error('Not implemented');
+    } 
+}
+
+
 module.exports = {
     findStringInSnakingPuzzle: findStringInSnakingPuzzle,
     getPermutations: getPermutations,
-    getMostProfitFromStockQuotes: getMostProfitFromStockQuotes
+    getMostProfitFromStockQuotes: getMostProfitFromStockQuotes,
+    UrlShortener: UrlShortener
 };
--- test/12-katas-3-tests.js
+++ test/12-katas-3-tests.js
@@ -99,4 +99,36 @@ describe('12-katas-3-tasks', function() {
     });
 
 
+    it.optional('urlShortener should return encoded string shorter than original url', () => {
+        [
+            'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/imul',
+            'https://en.wikipedia.org/wiki/Percent-encoding#Types_of_URI_characters',
+            'https://en.wikipedia.org/wiki/Binary-to-text_encoding#Encoding_plain_text'
+        ].forEach(data => {
+            var urlShortener = new tasks.UrlShortener();
+            var actual = urlShortener.encode(data);
+            assert(
+                data.length / actual.length > 1.5,
+                `urlShortener.encode for "${data}" returns "${actual}" that is only ${data.length/actual.length} times less than original url`
+            );
+        });
+    });
+
+
+    it.optional('urlShortener should decode shorten link to to the original url', () => {
+        [
+            'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/imul',
+            'https://www.example.com/catalog.html?search=mobile+phones&price=100-200&year=2016#top_links',
+        ].forEach(data => {
+            var urlShortener = new tasks.UrlShortener();
+            var encoded = urlShortener.encode(data);
+            var actual = urlShortener.decode(encoded);
+            assert.equal(
+                data,
+                actual,
+                `urlShortener.encode for "${data}" returns "${encoded}" but decode returns "${actual}"`
+            );
+        });
+    });
+
 });
