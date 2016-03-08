27200db6f9de99c38f51553fe60fb9c955e3919c
aorgish <aorgish>
2016-03-08 10:05:06 +0300

    Add getMostProfitFromStockQuotes kata

--- task/12-katas-3-tasks.js
+++ task/12-katas-3-tasks.js
@@ -47,8 +47,27 @@ function* getPermutations(chars) {
 }
 
 
+/**
+ * Returns the most profit from stock quotes.
+ * Stock quotes are stores in an array in order of date.
+ * The stock profit is the difference in prices in buying and selling stock.
+ * Each day, you can either buy one unit of stock, sell any number of stock units you have already bought, or do nothing. 
+ * Therefore, the most profit is the maximum difference of all pairs in a sequence of stock prices.
+ * 
+ * @param {array} quotes
+ * @return {number} max profit
+ *
+ * @example
+ *    [ 1, 2, 3, 4, 5, 6]   => 15  (buy at 1,2,3,4,5 and then sell all at 6)
+ *    [ 6, 5, 4, 3, 2, 1]   => 0   (nothing to buy)
+ *    [ 1, 6, 5, 10, 8, 7 ] => 18  (buy at 1,6,5 and sell all at 10)
+ */
+function getMostProfitFromStockQuotes(quotes) {
+    throw new Error('Not implemented');
+}
 
 module.exports = {
     findStringInSnakingPuzzle: findStringInSnakingPuzzle,
-    getPermutations: getPermutations
+    getPermutations: getPermutations,
+    getMostProfitFromStockQuotes: getMostProfitFromStockQuotes
 };
--- test/12-katas-3-tests.js
+++ test/12-katas-3-tests.js
@@ -35,7 +35,7 @@ describe('12-katas-3-tasks', function() {
     });
 
 
-    it.optional('getPermutations shoud return all possible string permutations', () => {
+    it.optional('getPermutations should return all possible string permutations', () => {
         [
             {
                 chars:    'a',
@@ -68,5 +68,35 @@ describe('12-katas-3-tasks', function() {
             'Number of 5 chars permutations should be 120.'
         );
     });
+
+
+    it.optional('getMostProfitFromStockQuotes should return the max profit from stock trading', () => {
+        [
+            {
+                quotes:   [ 1, 2, 3, 4, 5, 6 ],
+                expected: 15
+            },{
+                quotes:   [ 6, 5, 4, 3, 2, 1 ],
+                expected: 0
+            },{
+                quotes:   [ 1, 6, 5, 10, 8, 7 ],
+                expected: 18
+            },{
+                quotes:   [ 31, 312, 3, 35, 33, 3, 44, 123, 126, 2, 4, 1 ],
+                expected: 798
+            },{
+                quotes:   [ 1, 20, 1, 30, 1, 40, 1, 50, 1, 40, 1, 30, 1, 20, 1 ],
+                expected: 343
+            }
+        ].forEach(data => {
+            var actual = tasks.getMostProfitFromStockQuotes(data.quotes);
+            assert.equal(
+                actual,
+                data.expected,
+                `Most profit for [${data.quotes}] quotes is ${data.expected} but actually ${actual}`
+            );
+        });
+    });
+
 
 });
