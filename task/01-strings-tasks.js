3c85d0e6aa7490dbd1d30a1b7bad28d21c014af9
aorgish <aorgish>
2016-03-13 21:12:32 +0300

    Fix typo

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -42,7 +42,7 @@ function getStringLength(value) {
 }
 
 /**
- * Returns the result of string tepmlate and given parameters firstName and lastName.
+ * Returns the result of string template and given parameters firstName and lastName.
  * Please do not use concatenation, use template string :
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/template_strings
  *
