ff86d9ff949fe11eca79f158269004908fc861d1
Zakhar Sianko <Zakhar_Sianko@epam.com>
2016-03-14 16:15:18 +0300

    fix some wrong test examples

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -178,7 +178,7 @@ function getParallelipidedDiagonal(a,b,c) {
  *   1678, 0  => 1678
  *   1678, 1  => 1680
  *   1678, 2  => 1700
- *   1678, 3  => 1000
+ *   1678, 3  => 2000
  */
 function roundToPowerOfTen(num, pow) {
     throw new Error('Not implemented');
@@ -199,7 +199,7 @@ function roundToPowerOfTen(num, pow) {
  *   11 => true
  *   12 => false
  *   16 => false
- *   17 => false
+ *   17 => true
  */
 function isPrime(n) {
     throw new Error('Not implemented');
