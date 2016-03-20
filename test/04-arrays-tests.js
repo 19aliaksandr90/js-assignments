c82912086575e5a381775f141489a3afb6f671e6
aorgish <aorgish>
2016-03-20 20:49:35 +0300

    Add test to sortCitiesArray task to disable most frequently mistake with sort func

--- test/04-arrays-tests.js
+++ test/04-arrays-tests.js
@@ -594,6 +594,35 @@ describe('04-arrays-tasks', function() {
                    { country: 'D', city: '1' },
                    { country: 'E', city: '1' }
                 ]
+            },{
+                arr:  [
+                   { country: '5', city: '1' },
+                   { country: '1', city: '1' },
+                   { country: '1', city: '2' },
+                   { country: '1', city: '3' },
+                   { country: '2', city: '2' },
+                   { country: '1', city: '1' },
+                   { country: '1', city: '1' },
+                   { country: '2', city: '1' },
+                   { country: '3', city: '1' },
+                   { country: '3', city: '3' },
+                   { country: '2', city: '5' },
+                   { country: '5', city: '2' }
+                ],
+                expected: [
+                   { country: '1', city: '1' },
+                   { country: '1', city: '1' },
+                   { country: '1', city: '1' },
+                   { country: '1', city: '2' },
+                   { country: '1', city: '3' },
+                   { country: '2', city: '1' },
+                   { country: '2', city: '2' },
+                   { country: '2', city: '5' },
+                   { country: '3', city: '1' },
+                   { country: '3', city: '3' },
+                   { country: '5', city: '1' },
+                   { country: '5', city: '2' }
+                ]
             }
         ].forEach(data => {
             var actual = tasks.sortCitiesArray(data.arr);
